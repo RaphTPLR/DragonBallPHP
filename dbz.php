5926c8e2fb6177a46c879e3cb505722addb9ebcb
RaphTPLR <102389757+RaphTPLR@users.noreply.github.com>
2023-10-26 11:55:04 +0200

    feat: menu de test

--- dbz.php
+++ dbz.php
@@ -1,95 +1,152 @@
 <?php
- class Personnages {
+class Personnages
+{
     protected $nom;
     protected $puissance;
     protected $pv;
 
-    public function __construct($Nom,$Puissance,$Pv){
-        $this->nom=$Nom;
-        $this->puissance=$Puissance;
-        $this->pv=$Pv;
+    public function __construct($Nom, $Puissance, $Pv)
+    {
+        $this->nom = $Nom;
+        $this->puissance = $Puissance;
+        $this->pv = $Pv;
     }
 
-    public function getNom() {
+    public function getNom()
+    {
         return $this->nom;
     }
 
-    public function getPuissance(){
+    public function getPuissance()
+    {
         return $this->puissance;
     }
 
-    public function getPv(){
+    public function getPv()
+    {
         return $this->pv;
     }
 
-    public function setPv($newPv) {
+    public function setPv($newPv)
+    {
         $this->pv = $newPv;
     }
 
-    public function Attack($hero, $enemie) {
-        $attack = $hero->getPuissance();
+    public function Attack($enemie)
+    {
+        $attack = $this->getPuissance();
         $pv = $enemie->getPv();
         $update = $pv - $attack;
         if ($update > 0) {
-            $this->setPv($update);
-            echo "Il ne te reste plus que ", $enemie->getPv(), "à ", $enemie->getNom(), "!\n";
+            $enemie->setPv($update);
+            echo "Il ne te reste plus que ", $enemie->getPv(), " PV à ", $enemie->getNom(), "!\n";
         } else {
-            echo $enemie->getNom() ,"est mort !\n";
+            echo $enemie->getNom(), "est mort !\n";
         }
     }
- }
+}
 
- class Hero extends Personnages{
+class Hero extends Personnages
+{
     protected $attaque_hero;
 
-    public function __construct($Nom,$Puissance,$Pv,$Attaque_hero){
-        parent::__construct($Nom,$Puissance,$Pv);
+    public function __construct($Nom, $Puissance, $Pv, $Attaque_hero)
+    {
+        parent::__construct($Nom, $Puissance, $Pv);
         $this->attaque_hero = $Attaque_hero;
     }
 }
 
- class Vilain extends Personnages{
+class Vilain extends Personnages
+{
     protected $attaque_vilain;
-    public function __construct($Nom,$Puissance,$Pv,$Attaque_vilain) {
-        parent:: __construct($Nom,$Puissance,$Pv);
+    public function __construct($Nom, $Puissance, $Pv, $Attaque_vilain)
+    {
+        parent::__construct($Nom, $Puissance, $Pv);
         $this->attaque_vilain = $Attaque_vilain;
     }
- }
-  class Goku extends Hero {
-    public function __construct() {
+}
+class Goku extends Hero
+{
+    public function __construct()
+    {
         $this->nom = "Goku";
-        $this->puissance = 100;
+        $this->puissance = 50;
         $this->pv = 100;
-        $this->attaque_hero= "";
+        $this->attaque_hero = "";
 
     }
-  }
-  class Vegeta extends Hero {
-    public function __construct() {
+}
+class Vegeta extends Hero
+{
+    public function __construct()
+    {
         $this->nom = "Vegeta";
         $this->puissance = 100;
         $this->pv = 100;
         $this->attaque_hero = "";
-        
+
     }
-  }
-  class Freezer extends Vilain {
-    public function __construct() {
+}
+class Freezer extends Vilain
+{
+    public function __construct()
+    {
         $this->nom = "Freezer";
-        $this->puissance = 100 ;
+        $this->puissance = 100;
         $this->pv = 100;
         $this->attaque_vilain = "";
     }
-  }
-  class Cell extends Vilain {
-    public function __construct() {
+}
+class Cell extends Vilain
+{
+    public function __construct()
+    {
         $this->nom = "Cell";
-        $this->puissance = 100 ;
+        $this->puissance = 100;
         $this->pv = 100;
         $this->attaque_vilain = "";
     }
-  }
-  
-$vilain = new Vilain("Nom", 25, 200, "Feur");
-$vilain->getNom();
+}
+
+$goku = new Goku();
+$vegeta = new Vegeta();
+$freezer = new Freezer();
+$cell = new Cell();
+
+$a = 0;
+
+while ($a == 0) {
+    popen("cls", "w");
+    echo "Qui souhaites-tu voir ?\n1 - Goku\n2 - Vegeta\n3 - Freezer\n4 - Cell\n5 - Quitter\n\n";
+    $choice = readline("> ");
+    switch ($choice) {
+        case 1:
+            popen("cls", "w");
+            echo $goku->getNom(), " attaque !\n\n", $freezer->getPv(), "\n", $goku->Attack($freezer), "\n";
+            readline("> ");
+            break;
+        case 2:
+            popen("cls", "w");
+            echo $vegeta->getNom();
+            sleep(1);
+            break;
+        case 3:
+            popen("cls", "w");
+            echo $freezer->getNom();
+            sleep(1);
+            break;
+        case 4:
+            popen("cls", "w");
+            echo $cell->getNom();
+            sleep(1);
+            break;
+        case 5:
+            popen("cls", "w");
+            $a = 1;
+            break;
+        default:
+            echo "Ce n'est pas disponible !\n";
+    }
+}
 ?>
